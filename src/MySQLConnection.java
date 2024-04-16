54229e41f7df39d73b87caa1eb5167c2a127791d
LAB14_USER ON WS05 <L14Y15W05@citulab.net>
2024-04-16 15:54:15 +0800

    sql init done

--- src/MySQLConnection.java
+++ src/MySQLConnection.java
@@ -1,11 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MySQLConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/dbcsit228omasas";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
     static Connection getConnection() {
         Connection c = null;
-        c = DriverManager.getConnection();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Database Connection Success");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return c;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
     }
 }
